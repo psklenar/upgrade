15e9c60440cbff5a51a4907e5251eb1471ac45dc
petr sklenar <psklenar@gmail.com>
2026-04-09 13:35:57 +0200

    Enable Fedora repository during upgrade process

--- tasks/upgrade/task.sh
+++ tasks/upgrade/task.sh
@@ -18,6 +18,9 @@ rlJournalStart
             if dnf repolist | grep testing-farm-tag-repository; then
                 rlRun "dnf config-manager setopt testing-farm-tag-repository.enabled=0"
             fi
+            if dnf repolist | grep fedora; then
+                rlRun "dnf config-manager setopt fedora.enabled=1"
+            fi
             rlRun "dnf system-upgrade download --disablerepo='*beaker*' --releasever=$TARGET -y" 0 "Download new Fedora packages"
             rlRun "tmt-reboot -c \"dnf5 -y offline reboot\" -t 1800" 0 "Start actual upgrade"
         else
